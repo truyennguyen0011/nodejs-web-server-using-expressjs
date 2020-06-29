dbe878645f456f64255d1ae1409ce29653107a58
Truyen <nguyentruyen28101999@gmail.com>
2020-06-29 16:28:47 +0700

    add something

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const userRoutes = require('./routes/user.route');
+const authRoutes = require('./routes/auth.route')
+
+const authMiddleware = require('./middlewares/auth.middleware');
+
 const port = 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: 'Yours'
+    });
 });
 
-app.listen(port, function() {
+app.use('/users', authMiddleware.requireAuth, userRoutes);
+app.use('/auth', authRoutes);
+
+app.listen(port, function () {
     console.log('Server listening on port: ' + port);
 });
---
+++ routes/user.route.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controller/user.controller');
+const validate = require('../validate/user.validate');
+
+router.get('/', controller.index);
+
+router.get('/cookie', function (req, res, next) {
+    res.cookie('user-id', 12345);
+    res.send('Hello');
+});
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', validate.postCreate, controller.postCreate);
+
+module.exports = router;
